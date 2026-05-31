9b390a2d04ffd10cb1b83ff4fde82df1f518096a
Ifeoma <osegboifeoma3@gmail.com>
2026-05-31 13:09:47 +0200

    Add initial script.js file for frontend chat functionality

---
+++ Polling/Frontend/script.js
@@ -0,0 +1,10 @@
+const joinScreen = document.getElementById("joinScreen");
+const chatScreen = document.getElementById("chatScreen");
+
+const usernameInput = document.getElementById("usernameInput");
+const joinBtn = document.getElementById("joinBtn");
+
+const messagesContainer = document.getElementById("messages");
+
+const input = document.getElementById("messageInput");
+const sendBtn = document.getElementById("sendBtn");
